8b22ef35ba7caf12c9f764dbd05aaf984668d9f5
LIMJONGSOO <limjs4535@gmail.com>
2020-04-19 14:57:44 +0900

    dev function down, space button

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -6,31 +6,34 @@ import Score from '../Score/Score';
 import Tetromino from '../Tetromino/Tetromino';
 import Operator from '../Operator/Operator';
 
+export const tetrominoList = [
+    {type:'tetromino1', location:[[0,0],[1,0],[2,0],[3,0]]},
+    {type:'tetromino2', location:[[1,0],[2,0],[3,0],[3,1]]},
+    {type:'tetromino3', location:[[2,0],[2,1],[3,0],[3,1]]},
+    {type:'tetromino4', location:[[1,1],[2,1],[3,0],[3,1]]},
+    {type:'tetromino5', location:[[1,1],[2,0],[2,1],[3,0]]},
+    {type:'tetromino6', location:[[1,0],[2,0],[2,1],[3,1]]},
+    {type:'tetromino7', location:[[1,1],[2,0],[2,1],[3,1]]}
+];
+
 class Board extends Component {
     constructor(props) {
         super(props);
+
+        const newTetromino = Object.assign({},tetrominoList[~~(Math.random()*7)]);
+        newTetromino.location = newTetromino.location.map((location) => [location[0] - 4, location[1] + 4]);
+        const nextTetromino = Object.assign({},tetrominoList[~~(Math.random()*7)]);
+        nextTetromino.location = nextTetromino.location.map((location) => [location[0], location[1]]);
+
         this.state = {
-            nowTetromino: {},
-            nextTetromino: {},
+            rowCnt: 20,
+            colCnt: 10,
+            speed: 500,
+            nowTetromino: newTetromino,
+            nextTetromino: nextTetromino,
             testrisTable: Array.from(Array(20), () => Array.from(Array(10), () => 'empty')),
-            tetrominoList:[
-                {type:'tetromino1', location:[[0,0],[1,0],[2,0],[3,0]]},
-                {type:'tetromino2', location:[[1,0],[2,0],[3,0],[3,1]]},
-                {type:'tetromino3', location:[[2,0],[2,1],[3,0],[3,1]]},
-                {type:'tetromino4', location:[[1,1],[2,1],[3,0],[3,1]]},
-                {type:'tetromino5', location:[[1,1],[2,0],[2,1],[3,0]]},
-                {type:'tetromino6', location:[[1,0],[2,0],[2,1],[3,1]]},
-                {type:'tetromino7', location:[[1,1],[2,0],[2,1],[3,1]]}
-            ],
             score: 0
         };
-
-        const newTetromino = Object.assign({},this.state.tetrominoList[~~(Math.random()*7)]);
-        newTetromino.location = newTetromino.location.map((location) => [location[0] - 4, location[1] + 4]);
-        this.state.nowTetromino = newTetromino;
-        const nextTetromino = Object.assign({},this.state.tetrominoList[~~(Math.random()*7)]);
-        nextTetromino.location = nextTetromino.location.map((location) => [location[0], location[1]]);
-        this.state.nextTetromino = nextTetromino;
     }
 
     componentDidMount() {
@@ -43,9 +46,7 @@ class Board extends Component {
     downTetromino() {
         setTimeout(() => {
             if(!this.checkTouchBottom()) {
-                const downNowTetromino = this.state.nowTetromino;
-                downNowTetromino.location = this.state.nowTetromino.location.map((location) => [location[0] + 1, location[1]]);
-                this.setState({nowTetromino: downNowTetromino});
+                this.moveTetromino(1, 0); // 아래로 한칸 이동
             } else {
                 if(!this.checkTouchTop()) {
                     const testrisTable = this.state.testrisTable;
@@ -54,7 +55,7 @@ class Board extends Component {
                     });
                     const newTetromino = this.state.nextTetromino;
                     newTetromino.location = newTetromino.location.map((location) => [location[0] - 4, location[1] + 4]);
-                    const nextTetromino = Object.assign({},this.state.tetrominoList[~~(Math.random()*7)]);
+                    const nextTetromino = Object.assign({},tetrominoList[~~(Math.random()*7)]);
                     nextTetromino.location = nextTetromino.location.map((location) => [location[0], location[1]]);
                     this.setState({nowTetromino: newTetromino, nextTetromino: nextTetromino, testrisTable: testrisTable});
                 } else {
@@ -62,17 +63,72 @@ class Board extends Component {
                 }
             }
             this.downTetromino();
-        }, 100);
+        }, this.state.speed);
     }
 
     checkTouchBottom() {
         return this.state.nowTetromino.location
-                .filter((location) => location[0] + 1 >= 0 && (location[0] + 1 === 20 || this.state.testrisTable[location[0] + 1][location[1]] !== 'empty'))
+                .filter((location) => location[0] + 1 >= 0 && (location[0] + 1 === this.state.rowCnt || this.state.testrisTable[location[0] + 1][location[1]] !== 'empty'))
                 .length > 0 ?  true : false;
     }
 
     checkTouchTop() {
         return this.state.nowTetromino.location.filter((location) => location[0] < 0 ).length > 0 ? true : false;
+    }
+
+    checkTouchRight() {
+        return this.state.nowTetromino.location
+                .filter((location) => location[0] + 1 >= 0 && location[1] + 1 >= this.state.colCnt && (location[1] + 1 === this.state.colCnt || this.state.testrisTable[location[0]][location[1] + 1] !== 'empty'))
+                .length > 0 ?  true : false;
+    }
+
+    checkTouchLeft() {
+        return this.state.nowTetromino.location
+                .filter((location) => location[0] + 1 >= 0 && (location[1] === 0 || this.state.testrisTable[location[0]][location[1] - 1] !== 'empty'))
+                .length > 0 ?  true : false;
+    }
+
+    moveTetromino(y, x) {
+        const moveTetromino = this.state.nowTetromino;
+        moveTetromino.location = moveTetromino.location.map((location) => [location[0] + y, location[1] + x]);
+        this.setState({nowTetromino: moveTetromino});
+    }
+
+    moveRight = () => {
+        if (!this.checkTouchRight()) {
+            this.moveTetromino(0, 1);
+        }
+    }
+
+    moveLeft = () => {
+        if (!this.checkTouchLeft()) {
+            this.moveTetromino(0, -1);
+        }
+    }
+
+    moveDown = () => {
+        if(!this.checkTouchBottom()) {
+            this.moveTetromino(1, 0);
+        }
+    }
+
+    moveToBottom = () => {
+        if(!this.checkTouchBottom()) {
+            let moveCnt = 0;
+            const moveTetromino = Object.assign({},this.state.nowTetromino);
+
+            while(moveTetromino.location
+                .filter((location) => location[0] + 1 >= 0 && (location[0] + 1 === this.state.rowCnt || this.state.testrisTable[location[0] + 1][location[1]] !== 'empty'))
+                .length === 0) {
+                    moveCnt++;
+                    moveTetromino.location = moveTetromino.location.map((location) => [location[0] + 1, location[1]]);
+            }
+            this.moveTetromino(moveCnt, 0);
+        }
+    }
+
+    rotationTetromino = () => {
+        
     }
 
     render() {
@@ -83,7 +139,13 @@ class Board extends Component {
                     <Tetris testrisTable={this.state.testrisTable} nowTetromino={this.state.nowTetromino}/>
                     <Score score={this.state.score}/>
                     <Tetromino nextTetromino={this.state.nextTetromino}/>
-                    <Operator />
+                    <Operator 
+                        moveRight={this.moveRight} 
+                        moveLeft={this.moveLeft} 
+                        moveDown={this.moveDown} 
+                        moveToBottom={this.moveToBottom}
+                        rotationTetromino={this.rotationTetromino}
+                    />
                 </div>
             </div>
         );
